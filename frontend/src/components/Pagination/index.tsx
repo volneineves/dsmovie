9d11ba1f30ce9c1b256db07dc7931e4778d75ec0
Volnei Neves <volneinevesfilho@hotmail.com>
2022-05-15 14:05:11 -0300

    Ajustando setas da paginação

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -7,19 +7,19 @@ type Props = {
     onChange: Function;
 }
 
-function Pagination({ page, onChange } : Props) {
+function Pagination({ page, onChange }: Props) {
 
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" 
+                <button className="dsmovie-pagination-button"
                     disabled={page.first} onClick={() => onChange(page.number - 1)} >
-                    <Arrow className="dsmovie-flip-horizontal"/>
+                    <Arrow />
                 </button>
                 <p>{`${page.number + 1} de ${page.totalPages}`}</p>
-                <button className="dsmovie-pagination-button" 
+                <button className="dsmovie-pagination-button"
                     disabled={page.last} onClick={() => onChange(page.number + 1)} >
-                    <Arrow/>
+                    <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
         </div>
